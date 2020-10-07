53df9a9089e016d898b0f83ea8bb027189c27ac2
jeanamg92 <j34n.amg@gmail.com>
2020-10-07 17:52:28 -0300

    atividade pagina 23

--- js/atvScriptsPage23.js
+++ js/atvScriptsPage23.js
@@ -1,11 +1,14 @@
 const draggableElements = document.querySelectorAll(".draggable");
 const droppableElements = document.querySelectorAll(".dropable");
+
 const acertou = document.getElementById("acertou");
 const errou = document.getElementById("errou")
+
 const parabens = document.getElementById("parabens")
 const audio = new Audio('../audio/parabens.mp3')
 
 window.acertos = 0
+window.calculo = 1
 draggableElements.forEach(elem => {
     elem.addEventListener("dragstart", dragStart);
     elem.addEventListener("drag", drag);
@@ -18,15 +21,6 @@ droppableElements.forEach(elem => {
     elem.addEventListener("dragleave", dragLeave);
     elem.addEventListener("drop", drop);
 });
-
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-        if ((new Date().getTime() - start) > milliseconds) {
-            break;
-        }
-    }
-}
 
 // funcoes drag and drop
 function dragStart(event) {
@@ -57,19 +51,25 @@ function drop(event) {
     event.preventDefault();
 
     const draggableElementData = event.dataTransfer.getData("text")
-    const droppableElementData = event.target.getAttribute("result")
+    const droppableElementData = event.target.getAttribute("id")
+
+    console.log(draggableElementData)
+    console.log(droppableElementData)
 
     if (draggableElementData == droppableElementData && event.target.innerHTML == "") {
-        event.target.innerHTML = draggableElementData
+        event.target.innerHTML = document.getElementById(draggableElementData).innerHTML
         event.target.disable = "true"
         event.target.style.opacity = "0.7"
+
+        document.querySelector('.colunaElementos > #' + draggableElementData).classList.add('transparent')
+        document.querySelector('.colunaElementos > #' + draggableElementData).pointerEvents = "none"
 
         acertou.classList.remove("naoMostra")
         setTimeout(function() {
             acertou.classList.add("naoMostra")
         }, 1000);
+
         window.acertos++
-            console.log(window.acertos)
     } else if (!(draggableElementData == droppableElementData) && event.target.innerHTML == "") {
         errou.classList.remove("naoMostra")
         setTimeout(function() {
@@ -79,7 +79,26 @@ function drop(event) {
         return
     }
 
-    if (window.acertos == droppableElements.length) {
+    var elemento = this.parentElement.parentElement
+
+
+    if (window.acertos == 3 && !(elemento.id == 4)) {
+        window.acertos = 0
+
+        setTimeout(function() {
+            elemento.classList.add('up-outscreen-translated')
+            var nextOp = Number(elemento.id) + 1;
+            document.getElementById(nextOp).classList.remove('transparent', 'bottom-outscreen-translated')
+        }, 1250);
+
+    }
+
+    if (window.acertos == 4) {
+        setTimeout(function() {
+            elemento.classList.add('up-outscreen-translated')
+            var nextOp = Number(elemento.id) + 1;
+            document.getElementById(nextOp).classList.remove('transparent', 'bottom-outscreen-translated')
+        }, 1250);
         setTimeout(function() {
             audio.play()
             parabens.classList.remove("naoMostra")
@@ -90,4 +109,5 @@ function drop(event) {
             parabens.classList.add("naoMostra")
         }, 4000);
     }
+
 }
