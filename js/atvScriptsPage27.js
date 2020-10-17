73403d5d45e1999a37576da534dab94a3837e3d6
jeanamg92 <j34n.amg@gmail.com>
2020-10-17 04:47:01 -0300

    update script pag27

--- js/atvScriptsPage27.js
+++ js/atvScriptsPage27.js
@@ -79,6 +79,7 @@ droppableElements.forEach(elem => {
     });
 });
 
+
 function showParabens(onend = undefined) { //onend = função pra ser chamada depois que o parabens sumir
     document.getElementById('parabens-wrapper').classList.remove('hidden');
 
